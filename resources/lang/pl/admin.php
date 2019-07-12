9d7d1ce2a789ff103de9c1d2031530b95418e7fe
Bartłomiej Talarczyk <btalarczyk1@gmail.com>
2019-07-12 12:07:05 +0200

    basic admin view

---
+++ resources/lang/pl/admin.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Admin Dashboard Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'welcome' => 'Witaj w panelu admina, :Name!',
+    'aboutyou' => 'O Tobie',
+    'users' => 'Użytkownicy',
+    'flights' => 'Loty',
+
+];
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,18 +3,46 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        @if (session('status'))
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Admin Dashboard</div>
+
+                    <div class="card-body">
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+
+                        You are logged in!
+                    </div>
+                </div>
+            </div>
+        @endif
+        <div class="col-md-10">
             <div class="card">
-                <div class="card-header">Admin Dashboard</div>
-
+                <div class="card-header">{{ __('admin.welcome', ['name' => Auth::user()->name ]) }}</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <h2> {{ __('admin.aboutyou') }}</h2>
+                    <p>{{ Auth::user()->name . ' ' . Auth::user()->lastname }}<br/>
+                    {{ Auth::user()->country }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-6">
+            <div class="card">
+                <div class="card-header">{{ __('admin.users') }}</div>
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="card">
+                <div class="card-header">{{ __('admin.flights') }}</div>
+                <div class="card-body">
+                    
                 </div>
             </div>
         </div>
